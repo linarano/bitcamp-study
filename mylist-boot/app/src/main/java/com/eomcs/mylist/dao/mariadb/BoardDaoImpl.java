87e17c1084f758ed1ad620ab9b5a77824857cc84
linarano <ranothing@gmail.com>
2022-03-14 14:23:13 +0900

    .

--- mylist-boot/app/src/main/java/com/eomcs/mylist/dao/mariadb/BoardDaoImpl.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/dao/mariadb/BoardDaoImpl.java
@@ -10,7 +10,7 @@ import com.eomcs.mylist.domain.Board;
 
 // @Repository
 // - 클래스에 이 애노테이션을 붙여 표시해 두면, Spring Boot가 실행될 때 이 클래스의 객체를 자동 생성한다.
-// - 또한 이 객체를 원하는 곳에 자동으로 주입한다.
+// - 또한 이 객체를 원하는 곳에 자동으로 주입한다.........
 //
 @Repository  
 public class BoardDaoImpl implements BoardDao {
